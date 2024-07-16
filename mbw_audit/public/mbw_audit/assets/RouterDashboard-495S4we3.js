89e29541d418cec59b9e26c9595325cb80f53294
dev3.ekgis <dev3.ekgis@gmail.com>
2024-07-16 16:52:05 +0700

    fix base route

--- mbw_audit/public/mbw_audit/assets/RouterDashboard-495S4we3.js
+++ mbw_audit/public/mbw_audit/assets/RouterDashboard-495S4we3.js
@@ -1,4 +1,4 @@
-var Qo=Object.defineProperty;var ta=(i,t,e)=>t in i?Qo(i,t,{enumerable:!0,configurable:!0,writable:!0,value:e}):i[t]=e;var w=(i,t,e)=>(ta(i,typeof t!="symbol"?t+"":t,e),e);import{r as q,R as Nn,j as y,u as Wn,T as $n,c as Yn,s as ea,A as yi,H as ia,F as sa,a as vi,C as pt,D as na,b as oa,d as aa}from"./main-YpG99NP5.js";import{P as Mi}from"./progress-OsJomKPb.js";/*!
+var Qo=Object.defineProperty;var ta=(i,t,e)=>t in i?Qo(i,t,{enumerable:!0,configurable:!0,writable:!0,value:e}):i[t]=e;var w=(i,t,e)=>(ta(i,typeof t!="symbol"?t+"":t,e),e);import{r as q,R as Nn,j as y,u as Wn,T as $n,c as Yn,s as ea,A as yi,H as ia,F as sa,a as vi,C as pt,D as na,b as oa,d as aa}from"./main-OotvS9EX.js";import{P as Mi}from"./progress-NvRk2DBy.js";/*!
  * @kurkle/color v0.3.2
  * https://github.com/kurkle/color#readme
  * (c) 2023 Jukka Kurkela
